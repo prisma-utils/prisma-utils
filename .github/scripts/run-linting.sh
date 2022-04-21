95353bd7eb56b66a74709bbaa615db74251269a2
johannesschobel <johannes.schobel@googlemail.com>
2022-04-21 22:09:35 +0200

    chore: better ci

---
+++ .github/scripts/run-linting.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -o errexit -o noclobber -o nounset -o pipefail
+
+RUN_ALL=${1:-"False"}
+BASE=${2:-"origin/main"}
+
+echo "Running Linting"
+
+if [[ "$RUN_ALL" == "True" ]]; then
+  npm run affected:lint -- --all
+else
+  AFFECTED=$(node node_modules/.bin/nx affected:libs --plain --base="$BASE")
+  echo "Will Lint: $AFFECTED"
+  npm run affected:lint -- --base="$BASE"
+fi
+
+echo "Linting Complete"
+
+wait
---
+++ .github/scripts/run-testing.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -o errexit -o noclobber -o nounset -o pipefail
+
+RUN_ALL=${1:-"False"}
+BASE=${2:-"origin/main"}
+
+echo "Running Unit Testing"
+
+if [[ "$RUN_ALL" == "True" ]]; then
+  npm run affected:test -- --all --parallel --maxParallel=2
+else
+  AFFECTED=$(node node_modules/.bin/nx affected:libs --plain --base="$BASE")
+  echo "Will test: $AFFECTED"
+  npm run affected:test -- --base="$BASE" --parallel --maxParallel=2
+fi
+
+echo "Unit Testing Complete"
+
+wait
