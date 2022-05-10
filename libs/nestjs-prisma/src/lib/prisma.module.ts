1958cc5540ecdb7d3157a6da439e35b889681189
johannesschobel <johannes.schobel@googlemail.com>
2022-05-10 22:04:22 +0200

    chore: add config to nestjs-prisma

--- libs/nestjs-prisma/src/lib/prisma.module.ts
+++ libs/nestjs-prisma/src/lib/prisma.module.ts
@@ -1,6 +1,10 @@
-import { DynamicModule, Module } from '@nestjs/common';
+import { DynamicModule, Module, Provider } from '@nestjs/common';
+import {
+  PrismaModuleAsyncOptions,
+  PrismaModuleOptions,
+  PrismaOptionsFactory,
+} from './interfaces/prisma-module.options';
 import { PRISMA_SERVICE_OPTIONS } from './constants';
-import { PrismaModuleOptions } from './interfaces/prisma-module.options';
 import { PrismaService } from './prisma.service';
 
 @Module({
@@ -15,9 +19,56 @@ export class PrismaModule {
       providers: [
         {
           provide: PRISMA_SERVICE_OPTIONS,
-          useValue: PrismaService,
+          useValue: options.prismaServiceOptions,
         },
       ],
     };
+  }
+
+  static forRootAsync(options: PrismaModuleAsyncOptions): DynamicModule {
+    return {
+      global: options.isGlobal,
+      module: PrismaModule,
+      imports: options.imports || [],
+      providers: this.createAsyncProviders(options),
+    };
+  }
+
+  private static createAsyncProviders(
+    options: PrismaModuleAsyncOptions,
+  ): Provider[] {
+    if (options.useExisting || options.useFactory) {
+      return this.createAsyncOptionsProvider(options);
+    }
+
+    return [
+      ...this.createAsyncOptionsProvider(options),
+      {
+        provide: options.useClass,
+        useClass: options.useClass,
+      },
+    ];
+  }
+
+  private static createAsyncOptionsProvider(
+    options: PrismaModuleAsyncOptions,
+  ): Provider[] {
+    if (options.useFactory) {
+      return [
+        {
+          provide: PRISMA_SERVICE_OPTIONS,
+          useFactory: options.useFactory,
+          inject: options.inject || [],
+        },
+      ];
+    }
+    return [
+      {
+        provide: PRISMA_SERVICE_OPTIONS,
+        useFactory: async (optionsFactory: PrismaOptionsFactory) =>
+          await optionsFactory.createPrismaOptions(),
+        inject: [options.useExisting || options.useClass],
+      },
+    ];
   }
 }
