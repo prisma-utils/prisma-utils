60c04553c568a285240444d4930c65545060f697
johannesschobel <johannes.schobel@googlemail.com>
2022-05-11 16:55:46 +0200

    chore: update

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -5,7 +5,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [PrismaModule.forRoot({ isGlobal: true })],
+  imports: [
+    PrismaModule.forRoot({
+      isGlobal: true,
+      prismaServiceOptions: {
+        middlewares: [
+          async (params, next) => {
+            const before = Date.now();
+            const result = await next(params);
+            const after = Date.now();
+
+            console.log(
+              `Query ${params.model}.${params.action} took ${after - before}ms`,
+            );
+
+            return result;
+          },
+        ],
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
