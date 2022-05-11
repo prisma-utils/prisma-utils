80a70137ed55ab12b9904656708ecd3299764685
johannesschobel <johannes.schobel@googlemail.com>
2022-05-11 17:19:09 +0200

    chore: generate lib

---
+++ libs/nx-generators/src/generators/nx-generators/generator.spec.ts
@@ -0,0 +1,20 @@
+import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
+import { Tree, readProjectConfiguration } from '@nrwl/devkit';
+
+import generator from './generator';
+import { NxGeneratorsGeneratorSchema } from './schema';
+
+describe('nx-generators generator', () => {
+  let appTree: Tree;
+  const options: NxGeneratorsGeneratorSchema = { name: 'test' };
+
+  beforeEach(() => {
+    appTree = createTreeWithEmptyWorkspace();
+  });
+
+  it('should run successfully', async () => {
+    await generator(appTree, options);
+    const config = readProjectConfiguration(appTree, 'test');
+    expect(config).toBeDefined();
+  });
+});
