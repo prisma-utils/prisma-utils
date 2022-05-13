a835a1a5e3afbcc40d8f269a34c7019929d49a44
johannesschobel <johannes.schobel@googlemail.com>
2022-05-13 09:16:35 +0200

    chore: update nx

--- libs/nestjs-prisma/jest.config.ts
+++ libs/nestjs-prisma/jest.config.ts
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   displayName: 'nestjs-prisma',
 
   globals: {
@@ -12,5 +12,5 @@ module.exports = {
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/libs/nestjs-prisma',
-  preset: '../../jest.preset.ts',
+  preset: '../../jest.preset.js',
 };
--- libs/prismerge/jest.config.ts
+++ libs/prismerge/jest.config.ts
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   displayName: 'prismerge',
 
   globals: {
@@ -12,5 +12,5 @@ module.exports = {
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/libs/prismerge',
-  preset: '../../jest.preset.ts',
+  preset: '../../jest.preset.js',
 };
