23fa9d3cacb306e4690c74f9a175cc744f796a3b
johannesschobel <johannes.schobel@googlemail.com>
2022-05-13 09:16:35 +0200

    chore: update nx

--- libs/nx-generators/jest.config.ts
+++ libs/nx-generators/jest.config.ts
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   displayName: 'nx-generators',
 
   globals: {
@@ -11,5 +11,5 @@ module.exports = {
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/libs/nx-generators',
-  preset: '../../jest.preset.ts',
+  preset: '../../jest.preset.js',
 };
