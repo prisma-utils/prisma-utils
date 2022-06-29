f8aadee522812d6e46c32b946ab7f594b4dced59
johannesschobel <johannes.schobel@googlemail.com>
2022-06-29 16:20:39 +0200

    chore: bootstrap new lib

---
+++ libs/request-parser/README.md
@@ -0,0 +1,11 @@
+# request-parser
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build request-parser` to build the library.
+
+## Running unit tests
+
+Run `nx test request-parser` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/request-parser/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'request-parser',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/libs/request-parser',
+};
