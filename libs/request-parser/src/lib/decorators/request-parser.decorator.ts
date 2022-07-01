1c340df3723b0c1f8f68a130fa2e60bef84af969
johannesschobel <johannes.schobel@googlemail.com>
2022-07-01 12:19:38 +0200

    fix: trigger release

--- libs/request-parser/src/lib/decorators/request-parser.decorator.ts
+++ libs/request-parser/src/lib/decorators/request-parser.decorator.ts
@@ -5,7 +5,6 @@ import { RequestParserService } from '../services/request-parser.service';
 export const RequestParser = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
-
     return new RequestParserService().parseQuery(request.query);
   },
 );
