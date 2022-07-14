9f80d1ef32c64e9e8abeb78d27a668f8413cd2cb
johannesschobel <johannes.schobel@googlemail.com>
2022-07-14 09:51:26 +0200

    fix(request-parser): pass options to decorator

--- libs/request-parser/src/lib/decorators/request-parser.decorator.ts
+++ libs/request-parser/src/lib/decorators/request-parser.decorator.ts
@@ -1,10 +1,11 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { RequestQueryOptions } from '../models/request-query.options';
 
 import { RequestParserService } from '../services/request-parser.service';
 
 export const RequestParser = createParamDecorator(
-  (data: unknown, ctx: ExecutionContext) => {
+  (data: Partial<RequestQueryOptions>, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
-    return new RequestParserService().parseQuery(request.query);
+    return new RequestParserService().parseQuery(request.query, data);
   },
 );
