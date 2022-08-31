773acf05f9fc98311c2292c7e55902973f0a8783
johannesschobel <johannes.schobel@googlemail.com>
2022-08-31 15:58:58 +0200

    chore(repo): update commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,7 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-max-line-length': [2, 'always', 500],
+    'header-max-length': [2, 'always', 100],
+  },
+};
