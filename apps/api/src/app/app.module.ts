cd9d93ab2fd3b74a8ab3c9abea32c1a5c68094ac
johannesschobel <johannes.schobel@googlemail.com>
2023-08-16 23:26:55 +0200

    fix(api): fix issues

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,30 +1,10 @@
 import { Module } from '@nestjs/common';
-import { PrismaModule } from '@prisma-utils/nestjs-prisma';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    PrismaModule.forRoot({
-      isGlobal: true,
-      prismaServiceOptions: {
-        middlewares: [
-          async (params, next) => {
-            const before = Date.now();
-            const result = await next(params);
-            const after = Date.now();
-
-            console.log(
-              `Query ${params.model}.${params.action} took ${after - before}ms`,
-            );
-
-            return result;
-          },
-        ],
-      },
-    }),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -1,16 +1,13 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from '@prisma-utils/nestjs-prisma';
 
 @Injectable()
 export class AppService {
-  constructor(private prismaService: PrismaService) {}
-
   getData() {
     return { message: `Welcome to api!` };
   }
 
   async getCount() {
-    const count = this.prismaService.article.count();
+    const count = 5;
     return count;
   }
 }
